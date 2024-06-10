68518b36b06d7f4e75f3e2166dc9c7467b1e3589
waszo <kawaaramatofonn2934@gmail.com>
2024-06-10 19:25:47 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,104 @@
-var display = document.getElementById("output");
-
-function set(nums) {
- display.textContent += nums.textContent;
+function set(target) {
+     let result =  document.getElementById("output")
+     let target_value = target. innerHTML;
+     
+     
+    
+  if (target_value == "AC") {
+      result.innerHTML = "0" 
+    } else if (target_value == "=") {          
+        result.innerHTML = eval(result.innerHTML) 
+    } else if (target_value == "+" && result.innerHTML.slice(-1) == "+") {
+        let answer = result.innerHTML.slice(0, -1);
+        result.innerHTML = answer + "+";
+    } else if (target_value == "+" && result.innerHTML.slice(-1) == "-") {
+        let answer = result.innerHTML.slice(0, -1);
+        result.innerHTML = answer + "-";
+    } else if (target_value == "+" && result.innerHTML.slice(-1) == "*") {
+        let answer = result.innerHTML.slice(0, -1);
+        result.innerHTML = answer + "*";
+    } else if (target_value == "+" && result.innerHTML.slice(-1) == "/") {
+        let answer = result.innerHTML.slice(0, -1);
+        result.innerHTML = answer + "/";
+    } else if (target_value == "-" && result.innerHTML.slice(-1) == "-") {
+        let answer = result.innerHTML.slice(0, -1);
+        result.innerHTML = answer + "-";
+    } else if (target_value == "-" && result.innerHTML.slice(-1) == "+") {
+        let answer = result.innerHTML.slice(0, -1);
+        result.innerHTML = answer + "+";
+    } else if (target_value == "-" && result.innerHTML.slice(-1) == "*") {
+        let answer = result.innerHTML.slice(0, -1);
+        result.innerHTML = answer + "*";
+    } else if (target_value == "-" && result.innerHTML.slice(-1) == "/") {
+        let answer = result.innerHTML.slice(0, -1);
+        result.innerHTML = answer + "/";
+    } else if (target_value == "-" && result.innerHTML.slice(-1) == ".") {
+        let answer = result.innerHTML.slice(0, -1);
+        result.innerHTML = answer + ".";
+    } else if (target_value == "*" && result.innerHTML.slice(-1) == "*") {
+        let answer = result.innerHTML.slice(0, -1);
+        result.innerHTML = answer + "*";
+    } else if (target_value == "*" && result.innerHTML.slice(-1) == "+") {
+        let answer = result.innerHTML.slice(0, -1);
+        result.innerHTML = answer + "+";
+    } else if (target_value == "*" && result.innerHTML.slice(-1) == "-") {
+        let answer = result.innerHTML.slice(0, -1);
+        result.innerHTML = answer + "-";
+    } else if (target_value == "*" && result.innerHTML.slice(-1) == "/") {
+        let answer = result.innerHTML.slice(0, -1);
+        result.innerHTML = answer + "/";
+    } else if (target_value == "*" && result.innerHTML.slice(-1) == ".") {
+        let answer = result.innerHTML.slice(0, -1);
+        result.innerHTML = answer + ".";
+    } else if (target_value == "/" && result.innerHTML.slice(-1) == "/") {
+        let answer = result.innerHTML.slice(0, -1);
+        result.innerHTML = answer + "/";
+    } else if (target_value == "/" && result.innerHTML.slice(-1) == "+") {
+        let answer = result.innerHTML.slice(0, -1);
+        result.innerHTML = answer + "+";
+    } else if (target_value == "/" && result.innerHTML.slice(-1) == "-") {
+        let answer = result.innerHTML.slice(0, -1);
+        result.innerHTML = answer + "-";
+    } else if (target_value == "/" && result.innerHTML.slice(-1) == "*") {
+        let answer = result.innerHTML.slice(0, -1);
+        result.innerHTML = answer + "*";
+    } else if (target_value == "/" && result.innerHTML.slice(-1) == ".") {
+        let answer = result.innerHTML.slice(0, -1);
+        result.innerHTML = answer + ".";
+    } else if (target_value == "." && result.innerHTML.slice(-1) == ".") {
+        let answer = result.innerHTML.slice(0, -1);
+        result.innerHTML = answer + ".";
+    } else if (target_value == "." && result.innerHTML.slice(-1) == "+") {
+        let answer = result.innerHTML.slice(0, -1);
+        result.innerHTML = answer + "+";
+    } else if (target_value == "." && result.innerHTML.slice(-1) == "-") {
+        let answer = result.innerHTML.slice(0, -1);
+        result.innerHTML = answer + "-";
+    } else if (target_value == "." && result.innerHTML.slice(-1) == "*") {
+        let answer = result.innerHTML.slice(0, -1);
+        result.innerHTML = answer + "*";
+    } else if (target_value == "." && result.innerHTML.slice(-1) == "/") {
+        let answer = result.innerHTML.slice(0, -1);
+        result.innerHTML = answer + "/";
+    } else if(target_value == "."){
+       if(!result.innerHTML.toString().includes(".")){
+           result.innerHTML += ".";
+       }
+    } else {
+        if (result.innerHTML == "0") {
+            result.innerHTML = target_value
+        } else if (result.innerHTML == "00") {
+            result.innerHTML = target_value
+        } else { 
+            result. innerHTML += target_value
+        }
+        
+    }
+    if ("+-*/".indexOf(target_value.slice(-1))) {
+        
+    }
+     
 }
 
-function calc() {
- display.textContent = new Function ("return " +
- display.textContent)();
-}
 
-function reset() {
- display.textContent = "";
-}
+
